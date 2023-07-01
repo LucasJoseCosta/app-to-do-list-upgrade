3d7572aefa6ad41f2c3aac0e29114af314acadcd
Lucas Jose da Costa <lucjoc.1@hotmail.com>
2023-07-01 15:28:47 -0300

    Inicio projeto + toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { MaterialModule } from './shared/material/material.module';
+import { TaskListComponent } from './views/list/task-list/task-list.component';
+import { TaskItemComponent } from './views/list/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TaskListComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    //Material
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
